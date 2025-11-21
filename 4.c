1e6ca6b9b75a74ea7ac4feb6071edd6e4522da8b
sheikhzabeehullah-coder <sheikhzabihullah5@gmail.com>
2025-11-21 07:50:15 +0530

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() 
+{
+  int a, b, temp;
+  printf("Enter first number");
+  scanf("%d", &a);
+  printf("Enter second number");
+  scanf("%d", &b);
+  printf("Before swap: a = %d, b = %d
+", a, b);
+  temp = a;
+  a = b;
+  b = temp;
+  printf("After swap: a = %d, b = %d
+", a, b);
+  return 0;
+}

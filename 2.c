021d11eca4eda7ef3790546dd5cd1588d721880d
sheikhzabeehullah-coder <sheikhzabihullah5@gmail.com>
2025-11-21 07:44:56 +0530

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,1 +1,12 @@
-q
+#include <stdio.h>
+int main() 
+{
+  int a, b, sum;
+  printf("Enter first number: ");
+  scanf("%d", &a);
+  printf("Enter second number: ");
+  scanf("%d", &b);
+  sum = a + b;
+  printf("Sum is: %d", sum);
+  return 0;
+}

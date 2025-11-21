0157568fec86ac1298f10106c7d0b0f106704285
sheikhzabeehullah-coder <sheikhzabihullah5@gmail.com>
2025-11-21 07:47:11 +0530

    Update 3.c

--- 3.c
+++ 3.c
@@ -1,1 +1,16 @@
-a
+#include <stdio.h>
+int main() 
+{
+  int a, b;
+  printf("Enter first number");
+  scanf("%d", &a);
+  printf("Enter second number");
+  scanf("%d", &b);
+  if (a > b)
+    printf("%d is greater", a);
+  else if (a < b)
+    printf("%d is greater", b);
+  else
+    printf("Both are equal");
+  return 0;
+}
